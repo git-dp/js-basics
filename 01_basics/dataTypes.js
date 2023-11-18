e7ec2cff639c77c2af810a97ea7aed45dd5ec5e1
DP <95573677+git-dp@users.noreply.github.com>
2023-11-18 17:12:59 +0000

    useeditoras

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,12 @@
+/*
+PRIMITIVE 
+number - 2 to power 53
+string - ''
+boolean - true/false
+int 
+null - standalone value object(typeof)
+undefined - undefined (typeof)
+symbol 
+*/
+
+console.log(typeof(null));
