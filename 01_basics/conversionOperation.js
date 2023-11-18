a735b0366cf61f519e60702453026fc57149ce2d
DP <95573677+git-dp@users.noreply.github.com>
2023-11-18 17:46:32 +0000

    conversion of data types

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,15 @@
+let score = "abc";
+
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// NaN - not a number 
+// true - 1 , false  - 0
+
+let isLoggedIn = 1;
+let booleanIsLoggedIn= Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
