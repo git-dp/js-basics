ed6baea4279fabe6b967b21826d708a399b55ef0
DP <95573677+git-dp@users.noreply.github.com>
2023-11-18 18:21:38 +0000

    operations

--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -1,4 +1,4 @@
-let score = "abc";
+/* let score = "abc";
 
 // console.log(typeof(score));
 
@@ -13,3 +13,43 @@ let valueInNumber = Number(score)
 let isLoggedIn = 1;
 let booleanIsLoggedIn= Boolean(isLoggedIn);
 console.log(booleanIsLoggedIn);
+
+
+let someNumber = 33;
+let someString = String(someNumber);
+// console.log(someString);
+// console.log(typeof someString);
+
+// ****************************************************************** 
+// Operations 
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2**3);
+*/
+
+let str1 = "hello";
+let str2 = "darshan";
+let str3 = str1 + " " + str2; 
+// console.log(str3);
+
+// console.log(true);
+
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter);
+
+
+
+
+
+
+
+
+
+
