65fe5bff61b77e423fa3c1f77dd42d19aa41c016
DP <95573677+git-dp@users.noreply.github.com>
2023-11-19 06:33:10 +0000

    summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,18 @@
+// const id = Symbol("harry potter");
+// const anotherId = Symbol("harry potter");
+
+// console.log(id === anotherId);
+
+// const bigNumber = 957457368547875766687548754732323237n;
+// console.log(typeof(bigNumber));
+
+// non P / reference types = array, object, function
+
+const hero = ["a","b","c"];
+console.log(typeof hero);
+
+const zero = function(){
+        name = "a1";
+        age = 21;
+}
+console.log(typeof zero);
